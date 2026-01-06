8c57c4669d82d9789a7b88d44c2afc3498b9b836
bvernu <bvernu@uci.edu>
2026-01-05 20:10:33 -0500

    clean deploy of built site

--- src/Components/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import About from "./About";
-
-export default function AboutPage() {
-  return (
-    <div className="min-h-screen">
-      <About />
-    </div>
-  );
-}
--- src/Components/ProjectsPage.tsx
+++
@@ -1,9 +0,0 @@
-import Projects from "./Projects";
-
-export default function ProjectsPage() {
-  return (
-    <div className="min-h-screen">
-      <Projects />
-    </div>
-  );
-}
